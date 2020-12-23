4ce9e016edbebe8305c195e733e25bf844932f49
ilyadery <ilyadrbn@gmail.com>
2020-12-23 20:32:33 +0200

    2DPlatformer
    ЗАлил на гит

---
+++ Platformer/Assets/Scripts/BackgroundScripts/BGLoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGLoop : MonoBehaviour
+{
+    public float speed = 0.1F;
+    private Vector2 offset = Vector2.zero;
+    private Material material;
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = material.GetTextureOffset("_MainTex");
+    }
+
+   
+    void Update()
+    {
+        offset.x += speed * Time.deltaTime;
+        material.SetTextureOffset("_MainTex", offset);
+    }
+}
---
+++ Platformer/Assets/Scripts/BackgroundScripts/Background.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var height = Camera.main.orthographicSize * 2.0F;
+        var width = height * Screen.width / Screen.height;
+
+        if (gameObject.name == "Backgound1")
+        {
+            transform.localScale = new Vector3(width, height, 0);
+        }
+        else
+        {
+            transform.localScale = new Vector3(width + 3f, 5, 0);
+        }
+    }
+
+    
+}
